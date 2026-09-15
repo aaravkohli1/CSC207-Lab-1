dec54caf5e3582c18cfcdcf5ddc8979e22f58ed7
Aarav <aaravkohli2008@gmail.com>
2026-09-15 14:07:20 -0400

    finish lab

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,8 +7,8 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
-
+        int i = 1;
+        while (i <= 100){
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
@@ -31,6 +31,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
